7d5f863a3b5856c886c8efe841d66852380e37d1
Cristi Ivan <georgecristian.ivan@outlook.com>
2014-12-13 14:01:33 +0200

    create interest model

--- fb/admin.py
+++ fb/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from fb.models import UserPost, UserProfile
+from fb.models import UserPost, UserProfile, Interest
 
 
 admin.site.register(UserPost)
 admin.site.register(UserProfile)
+admin.site.register(Interest)
